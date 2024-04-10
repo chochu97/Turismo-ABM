aa03ddb3ce76b1debab715466469cb0c096555c2
nicok <nicokawaii97@gmail.com>
2024-04-10 00:41:28 -0300

    changes

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace UIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.comboTurismo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.txtIDEliminar = new System.Windows.Forms.TextBox();
+            this.txtIDMod = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 338);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(44, 387);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(107, 28);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar Entrada";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(337, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(412, 182);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nueva Entrada:";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(18, 187);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(133, 20);
+            this.txtDNI.TabIndex = 4;
+            // 
+            // comboTurismo
+            // 
+            this.comboTurismo.FormattingEnabled = true;
+            this.comboTurismo.Location = new System.Drawing.Point(17, 109);
+            this.comboTurismo.Name = "comboTurismo";
+            this.comboTurismo.Size = new System.Drawing.Size(134, 21);
+            this.comboTurismo.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Entradas Vendidas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DNI / Pasaporte:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Monto:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tipo de Turismo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Fecha Validez:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(334, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Eliminar Entrada, ingrese ID;";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(561, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(210, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Modificar Fecha de Vigencia, ingrese el ID:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(561, 338);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Nueva Fecha:";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(564, 367);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 14;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(337, 354);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(93, 33);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Eliminar Entrada";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(612, 405);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(97, 33);
+            this.btnModificar.TabIndex = 16;
+            this.btnModificar.Text = "Modificar Fecha";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(18, 260);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(133, 20);
+            this.txtMonto.TabIndex = 17;
+            // 
+            // txtIDEliminar
+            // 
+            this.txtIDEliminar.Location = new System.Drawing.Point(337, 302);
+            this.txtIDEliminar.Name = "txtIDEliminar";
+            this.txtIDEliminar.Size = new System.Drawing.Size(177, 20);
+            this.txtIDEliminar.TabIndex = 18;
+            // 
+            // txtIDMod
+            // 
+            this.txtIDMod.Location = new System.Drawing.Point(564, 298);
+            this.txtIDMod.Name = "txtIDMod";
+            this.txtIDMod.Size = new System.Drawing.Size(165, 20);
+            this.txtIDMod.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.Controls.Add(this.txtIDMod);
+            this.Controls.Add(this.txtIDEliminar);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboTurismo);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.ComboBox comboTurismo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.TextBox txtIDEliminar;
+        private System.Windows.Forms.TextBox txtIDMod;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIP
+{
+    public partial class Form1 : Form
+    {
+        private EntradaBusiness entradaBusiness = new EntradaBusiness();
+        private TipoTurismoBusiness turismobusy = new TipoTurismoBusiness();
+        public Form1()
+        {
+            InitializeComponent();
+            ActualizarGrilla();
+            ActualizarCombo();
+        }
+
+        public void ActualizarGrilla()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = entradaBusiness.GetEntradas();
+        }
+
+        public void ActualizarCombo()
+        {
+            comboTurismo.DataSource = null;
+            comboTurismo.DataSource = turismobusy.GetTurismos();
+
+            comboTurismo.DisplayMember = "nombre";
+            comboTurismo.ValueMember = "id_tipoturismo";
+            comboTurismo.DisplayMember = "nombre";
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Entrada entrada = new Entrada();
+            entrada.id_tipoturismo = Convert.ToInt32(comboTurismo.SelectedValue);
+            entrada.dni = Convert.ToInt32(txtDNI.Text);
+            entrada.monto = Convert.ToDouble(txtMonto.Text);
+            entrada.fecha_registro = DateTime.Now;
+            entrada.fecha_validez = dateTimePicker1.Value;
+
+            try
+            {
+                entradaBusiness.GuardarEntrada(entrada);
+                ActualizarGrilla();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                entradaBusiness.EliminarEntrada(Convert.ToInt32(txtIDEliminar.Text));
+                ActualizarGrilla();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                entradaBusiness.ModificarEntrada(Convert.ToInt32(txtIDMod.Text), dateTimePicker2.Value);
+                ActualizarGrilla();
+            }
+            catch( Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
